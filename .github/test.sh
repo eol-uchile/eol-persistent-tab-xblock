78b26270fe729b3d1f7c94b66b46052a02043dfa
matsalinas <matias.salinas.s@mail.pucv.cl>
2020-06-17 16:52:00 +0000

    Tests

---
+++ .github/test.sh
@@ -0,0 +1,15 @@
+#!/bin/dash
+
+pip install -e /openedx/requirements/eol-persistent-tab-xblock
+
+cd /openedx/requirements/eol-persistent-tab-xblock/eolpersistenttab
+cp /openedx/edx-platform/setup.cfg .
+mkdir test_root
+cd test_root/
+ln -s /openedx/staticfiles .
+
+cd /openedx/requirements/eol-persistent-tab-xblock/eolpersistenttab
+
+DJANGO_SETTINGS_MODULE=lms.envs.test EDXAPP_TEST_MONGO_HOST=mongodb pytest tests.py
+
+rm -rf test_root
