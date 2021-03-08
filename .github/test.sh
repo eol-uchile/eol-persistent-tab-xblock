11aea38025fe2fa53446c0b864bba890dddc2af6
matsalinas <matsalinas@uchile.cl>
2021-03-08 21:02:21 +0000

    Update tests koa

--- .github/test.sh
+++ .github/test.sh
@@ -2,14 +2,14 @@
 
 pip install -e /openedx/requirements/eol-persistent-tab-xblock
 
-cd /openedx/requirements/eol-persistent-tab-xblock/eolpersistenttab
+cd /openedx/requirements/eol-persistent-tab-xblock
 cp /openedx/edx-platform/setup.cfg .
 mkdir test_root
 cd test_root/
 ln -s /openedx/staticfiles .
 
-cd /openedx/requirements/eol-persistent-tab-xblock/eolpersistenttab
+cd /openedx/requirements/eol-persistent-tab-xblock
 
-DJANGO_SETTINGS_MODULE=lms.envs.test EDXAPP_TEST_MONGO_HOST=mongodb pytest tests.py
+DJANGO_SETTINGS_MODULE=lms.envs.test EDXAPP_TEST_MONGO_HOST=mongodb pytest eolpersistenttab/tests.py
 
 rm -rf test_root
